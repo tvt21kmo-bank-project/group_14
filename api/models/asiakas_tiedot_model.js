0f561424a9af48b91590d513ee775cd1cfe3a6da
AB4380 <ab4380@student.jamk.fi>
2021-11-23 13:13:19 +0200

    Kaikki siirrot ja luottorajat toimivat + idTili näkyy asikastiedoissa

--- api/models/asiakas_tiedot_model.js
+++ api/models/asiakas_tiedot_model.js
@@ -2,7 +2,7 @@ const db = require('../database');
 
 const asiakas_tiedot={
   get: function(callback) {
-    return db.query('select Etunimi, Sukunimi, Korttinumero, Tilinumero, Kortin_tyyppi, Saldo, Luottoraja from asiakas join kortti on asiakas.idAsiakas = kortti.idAsiakas join tili on tili.idKortti = kortti.idKortti;', 
+    return db.query('select Etunimi, Sukunimi, Korttinumero, idTili, Tilinumero, Kortin_tyyppi, Saldo, Luottoraja from asiakas join kortti on asiakas.idAsiakas = kortti.idAsiakas join tili on tili.idKortti = kortti.idKortti;', 
     callback);
   }
 }
