31f5a44a17249ab2275dc55fbf74575f04d065b4
AB4380 <ab4380@student.jamk.fi>
2021-12-01 17:45:22 +0200

    Kirjautuminen toimii, PIN on salattu, aloitettu tilitapahtuma näyttö

--- BankProject/qtBank_project/tilintapahtumat.h
+++ BankProject/qtBank_project/tilintapahtumat.h
@@ -1,6 +1,10 @@
 #ifndef TILINTAPAHTUMAT_H
 #define TILINTAPAHTUMAT_H
 
+#include <QMainWindow>
+#include <QtNetwork>
+#include <QNetworkAccessManager>
+#include <QJsonDocument>
 #include <QWidget>
 
 namespace Ui {
@@ -20,8 +24,14 @@ private slots:
 
     void on_pushButton_Kirjaudu_ulos_clicked();
 
+    void getTilitapahtumaSlot (QNetworkReply *tilitapahtuma_reply);
+
+    void on_pushButton_HaeTilitapahtumat_clicked();
+
 private:
     Ui::Tilintapahtumat *ui;
+    QNetworkAccessManager *tilitapahtuma_Manager;
+    QNetworkReply *tilitapahtuma_reply;
 };
 
 #endif // TILINTAPAHTUMAT_H
